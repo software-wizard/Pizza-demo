37620867d8cbe6226a8cd64688b17cccca056016
software-wizard <przemek@software-wizard.pl>
2020-02-14 21:51:03 +0100

    first implementation

--- model/src/main/java/pl/psi/pizza/Pizza.java
+++ model/src/main/java/pl/psi/pizza/Pizza.java
@@ -1,4 +1,11 @@
 package pl.psi.pizza;
 
 public class Pizza {
+    public double getCost() {
+        return 13.50;
+    }
+
+    public String getName() {
+        return "Margeritta na cienkim cieście";
+    }
 }
