cf5367166bae1fdc22b8ba7706dce4ad57e3f27c
software-wizard <przemek@software-wizard.pl>
2020-02-14 06:22:02 +0100

    initialize modules

---
+++ model/src/test/java/pl/psi/pizza/ItalianPizzaFactoryTest.java
@@ -0,0 +1,14 @@
+package pl.psi.pizza;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItalianPizzaFactoryTest {
+
+    @Test
+    void test(){
+        assertEquals(1,1);
+    }
+
+}
