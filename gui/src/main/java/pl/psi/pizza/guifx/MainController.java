0732a40bcadeaf59ae82ba043cfb796b4b831485
software-wizard <przemek@software-wizard.pl>
2020-02-24 07:46:44 +0100

    gui temp

--- gui/src/main/java/pl/psi/pizza/guifx/MainController.java
+++ gui/src/main/java/pl/psi/pizza/guifx/MainController.java
@@ -1,45 +1,73 @@
 package pl.psi.pizza.guifx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import pl.psi.menu.Menu;
+import pl.psi.menu.MenuFactory;
 import pl.psi.pizza.model.AmericanPiePizzaMenu;
 import pl.psi.pizza.model.ItalianPiePizzaMenu;
 
-public class MainController {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainController {
     @FXML
-    private VBox centerBox;
+    private Pane orderWrapper;
     @FXML
-    private Button confirmOrderButton;
+    private Pane MenuChooserWrapper;
     @FXML
-    private Label orderPriceLabel;
-    @FXML
-    private ToggleGroup pieGroup;
+    private Pane MenuWrapper;
+
+    private Menu currentMenu;
 
     @FXML
     private void initialize() {
-        orderPriceLabel.setText(" Tutaj powinny pojawić się zamówione pizze + cena ");
-        pieGroup.selectedToggleProperty().addListener( (e) -> refreshMenu());
+        orderWrapper.getChildren().add(new Label ("orderWrapper"));
+        orderWrapper.getChildren().add(new Label ("orderWrapper2"));
+
+        initMenuChooser();
+
+        MenuWrapper.getChildren().add(new Label ("MenuWrapper"));
+        MenuWrapper.getChildren().add(new Label ("MenuWrapper2"));
+
         refreshMenu();
     }
 
+    private void initMenuChooser() {
+        List<Menu> menus = new ArrayList<Menu>();
+        menus.add(MenuFactory.createMenu(MenuFactory.ADDITIONAL));
+        menus.add(MenuFactory.createMenu(MenuFactory.ITALIAN_PIZZA));
+        menus.add(MenuFactory.createMenu(MenuFactory.AMERICAN_PIZZA));
+        switch ()
+        MenuChooserWrapper.getChildren().add(new Label ("MenuChooserWrapper"));
+        ToggleGroup x = new ToggleGroup();
+        RadioButton a = new RadioButton(MenuFactory.ADDITIONAL);
+        a.setToggleGroup(x);
+        a.addEventFilter();
+        RadioButton b = new RadioButton(MenuFactory.ITALIAN_PIZZA);
+        b.setToggleGroup(x);
+        MenuChooserWrapper.getChildren().add(a);
+        MenuChooserWrapper.getChildren().add(b);
+
+
+    }
+
     private void refreshMenu(){
-        centerBox.getChildren().clear();
-        String selectedRationText = ((RadioButton) pieGroup.getSelectedToggle()).getText();
-        if ("Na cienkim".equals(selectedRationText)) {
-            ItalianPiePizzaMenu italianMenu = new ItalianPiePizzaMenu();
-            for (String pizza: italianMenu.getPizzasList() ) {
-                centerBox.getChildren().add(new PizzaEntry(pizza));
-            }
-        } else if ("Na grubym".equals(selectedRationText)) {
-            AmericanPiePizzaMenu americanMenu = new AmericanPiePizzaMenu();
-            for (String pizza: americanMenu.getPizzasList() ) {
-                centerBox.getChildren().add(new PizzaEntry(pizza));
-            }
-        }
+//        w zależności od klika wziac odpowiednie menu z fabryki
+//        centerBox.getChildren().clear();
+//        String selectedRationText = ((RadioButton) pieGroup.getSelectedToggle()).getText();
+//        if ("Na cienkim".equals(selectedRationText)) {
+//            ItalianPiePizzaMenu italianMenu = new ItalianPiePizzaMenu();
+//            for (String pizza: italianMenu.getPizzasList() ) {
+//                centerBox.getChildren().add(new PizzaEntry(pizza));
+//            }
+//        } else if ("Na grubym".equals(selectedRationText)) {
+//            AmericanPiePizzaMenu americanMenu = new AmericanPiePizzaMenu();
+//            for (String pizza: americanMenu.getPizzasList() ) {
+//                centerBox.getChildren().add(new PizzaEntry(pizza));
+//            }
+//        }
     }
 }
